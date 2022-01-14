2b4e1868804bb07674ed78bb6a755dfd36c12974
quantum-hash <96754248+quantum-hash@users.noreply.github.com>
2022-01-14 22:08:39 +0100

    Create high_quality_img_convert.py

---
+++ high_quality_img_convert.py
@@ -0,0 +1,5 @@
+"""
+• high_quality_img_convert
+• using Pillow (This library provides extensive file format support, an efficient internal representation, and fairly powerful image processing capabilities.)
+• pip install Pillow
+"""
